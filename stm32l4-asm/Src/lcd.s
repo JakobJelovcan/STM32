8effdb8355c5a93cee220aba8a7250d7bb419995
Jakob Jelovcan <Jakob.Jelovcan@outlook.com>
2023-01-14 20:25:00 +0100

    l4 lcd

--- stm32l4-asm/Src/lcd.s
+++ stm32l4-asm/Src/lcd.s
@@ -1,66 +1,157 @@
-	.syntax unified
-	.cpu cortex-m4
-	.fpu softvfp
-	.thumb
+.syntax unified
+.cpu cortex-m4
+.fpu softvfp
+.thumb
 
-	.equ RCC_BASE,			0x40021000
-	.equ RCC_AHB2ENR, 		0x4C
+.equ RCC_BASE,			0x40021000
+.equ RCC_AHB2ENR, 		0x4C
+.equ RCC_APB1ENR1,		0x58
 
-	.equ GPIOA_BASE, 		0x48000000
-	.equ GPIOB_BASE,		0x48000400
-	.equ GPIOC_BASE,		0x48000800
-	.equ GPIOD_BASE,		0x48000C00
-	.equ GPIOE_BASE,		0x48001000
-	.equ GPIOx_MODER,		0x00
-	.equ GPIOx_MODER_VALUE, 0xFFFFF300
-	.equ GPIOx_AFRL,		0x20
-	.equ GPIOx_AFRH,		0x24
+.equ GPIOA_BASE, 		0x48000000
+.equ GPIOB_BASE,		0x48000400
+.equ GPIOC_BASE,		0x48000800
+.equ GPIOD_BASE,		0x48000C00
+.equ GPIOE_BASE,		0x48001000
 
-	.equ LCD_BASE, 			0x40002400
-	.equ LCD_CR,			0x00
-	.equ LCD_FCR,			0x04
-	.equ LCD_SR,			0x08
-	.equ LCD_CLR,			0x0C
-	.equ LCD_RAM,			0x14
+.equ GPIOx_MODER,		0x00
+.equ GPIOx_PUPDR,		0x0C
+.equ GPIOx_AFRL,		0x20
+.equ GPIOx_AFRH,		0x24
+
+.equ LCD_BASE, 			0x40002400
+.equ LCD_CR,			0x00
+.equ LCD_FCR,			0x04
+.equ LCD_SR,			0x08
+.equ LCD_CLR,			0x0C
+.equ LCD_RAM,			0x14
 
 
-	.type	init_lcd %function
-	.global	init_lcd
-
-init_lcd:
-	push { r4, r5, lr }
-
-	ldr r4, =RCC_BASE
-
-	ldr r5, [r4, #RCC_AHB2ENR]
-	orr r5, #0x1F
-	str r5, [r4, #RCC_AHB2ENR]
+.type	init_lcd_gpio %function
+.global	init_lcd_gpio
+init_lcd_gpio:
+	push { lr }
 
 	bl init_lcd_a
 	bl init_lcd_b
 	bl init_lcd_c
-	bl init_lcd_d
-	bl init_lcd_e
+
+	pop { pc }
+
+
+.type	init_lcd %function
+.global	init_lcd
+init_lcd:
+	push { r4, r5, r6, r7, lr }
+
+	//Enable lcd clock
+	ldr r4, =RCC_BASE
+	ldr r5, =(1 << 9)
+	str r5, [r4, #RCC_APB1ENR1]
+
+	//Set GPIO A,B,C to AF11
+	bl init_lcd_gpio
+
+	bl lcd_off			//Turn off lcd
+	bl lcd_wait_off		//Wait for lcd to be turned off
+	bl lcd_clear_ram	//Clear ram
+
+	ldr r4, =LCD_BASE
+	ldr r5, =0x00001C00
+	str r5, [r4, #LCD_FCR]
+
+	ldr r5, =0x0000004C
+	str r5, [r4, #LCD_CR]
+
+	pop { r4, r5, r6, r7, pc }
+
+
+.type	lcd_off %function
+.global
+lcd_off:
+	push { r4, r5, lr }
+
+	ldr r4, =LCD_BASE
+	ldr r5, [r4, #LCD_CR]
+	bic r5, #(1 << 0)
+	str r5, [r4, #LCD_CR]
 
 	pop { r4, r5, pc }
 
 
-	.type	refresh_lcd %function
-	.global	refresh_lcd
+.type	lcd_on %function
+.global
+lcd_on:
+	push { r4, r5, lr }
 
-refresh_lcd:
-	push { r4, r5, r6, lr }
+	ldr r4, =LCD_BASE
+	ldr r5, [r4, #LCD_CR]
+	orr r5, #(1 << 0)
+	str r5, [r4, #LCD_CR]
+
+	pop { r4, r5, pc }
 
 
+.type	lcd_wait_off %function
+lcd_wait_off:
+	push { r4, r5, lr }
 
-	pop { r4, r5, r6, pc }
+	ldr r4, =LCD_BASE
+lcd_wait_off_loop:
+	ldr r5, [r4, #LCD_SR]
+	tst r5, #(1 << 0)
+	bne lcd_wait_off_loop
+
+	pop { r4, r5, pc }
+
+.type	lcd_set_contrast %function
+.global	lcd_set_contrast
+lcd_set_contrast:
+	push { r4, r5, lr }
+
+	ldr r4, =LCD_BASE
+	ldr r5, =0x00000007
+	and r0, r5
+	lsl r0, #10
+	ldr r5, [r4, #LCD_FCR]
+	orr r5, r0
+	str r5, [r4, #LCD_FCR]
+
+	pop { r4, r5, pc }
+
+.type	lcd_display_bar %function
+.global	lcd_display_bar
+lcd_display_bar:
+	push { r4, r5, lr }
+
+	ldr r4, =LCD_BASE
+wait_udr:
+	ldr r5, [r4, #LCD_SR]
+	tst r5, #(1 << 2)
+	bne wait_udr
+
+	ldr r5, =0x00FFFFFF
+	str r5, [r4, #LCD_RAM]
+	str r5, [r4, #LCD_RAM + 8]
+	str r5, [r4, #LCD_RAM + 16]
+	str r5, [r4, #LCD_RAM + 24]
+
+	ldr r5, =(1 << 2)
+	str r5, [r4, #LCD_SR]
+
+	pop { r4, r5, pc }
 
 
-	.type	init_lcd_a %function
-	.global init_lcd_a
+.type	init_lcd_a %function
+.global init_lcd_a
 init_lcd_a:
 	push { r4, r5, r6, lr }
 
+	ldr r4, =RCC_BASE
+	ldr r5, [r4, #RCC_AHB2ENR]
+	orr r5, #(1 << 0)
+	str r5, [r4, #RCC_AHB2ENR]
+
+	//PA1, PA2, PA3, PA6, PA7, PA8, PA9, PA10, PA15
 	ldr r4, =GPIOA_BASE
 	ldr r5, [r4, #GPIOx_MODER]
 	ldr r6, =0x3FC00F03
@@ -68,6 +159,11 @@ init_lcd_a:
 	ldr r6, =0x802AA0A8
 	orr r5, r6
 	str r5, [r4, #GPIOx_MODER]
+
+	ldr r5, [r4, #GPIOx_PUPDR]
+	ldr r6, =0x3FC00F03
+	and r5, r6
+	str r5, [r4, #GPIOx_PUPDR]
 
 	ldr r5, [r4, #GPIOx_AFRL]
 	ldr r6, =0x00FF000F
@@ -86,11 +182,17 @@ init_lcd_a:
 	pop { r4, r5, r6, pc }
 
 
-	.type	init_lcd_b %function
-	.global	init_lcd_b
+.type	init_lcd_b %function
+.global	init_lcd_b
 init_lcd_b:
 	push { r4, r5, r6, lr }
 
+	ldr r4, =RCC_BASE
+	ldr r5, [r4, #RCC_AHB2ENR]
+	orr r5, #(1 << 1)
+	str r5, [r4, #RCC_AHB2ENR]
+
+	//PB0, PB1, PB3, PB4, PB5, PB7, PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15
 	ldr r4, =GPIOB_BASE
 	ldr r5, [r4, #GPIOx_MODER]
 	ldr r6, =0x00003030
@@ -98,6 +200,11 @@ init_lcd_b:
 	ldr r6, =0xAAAA8A8A
 	orr r5, r6
 	str r5, [r4, #GPIOx_MODER]
+
+	ldr r5, [r4, #GPIOx_PUPDR]
+	ldr r6, =0x00003030
+	and r5, r6
+	str r5, [r4, #GPIOx_PUPDR]
 
 	ldr r5, [r4, #GPIOx_AFRL]
 	ldr r6, =0x0F000F00
@@ -116,84 +223,49 @@ init_lcd_b:
 	pop { r4, r5, r6, pc }
 
 
-	.type	init_lcd_c %function
-	.global init_lcd_c
+.type	init_lcd_c %function
+.global init_lcd_c
 init_lcd_c:
 	push { r4, r5, r6, lr }
 
+	ldr r4, =RCC_BASE
+	ldr r5, [r4, #RCC_AHB2ENR]
+	orr r5, #(1 << 2)
+	str r5, [r4, #RCC_AHB2ENR]
+
+	//PC0, PC1, PC2, PC3, PC4, PC5
 	ldr r4, =GPIOC_BASE
 	ldr r5, [r4, #GPIOx_MODER]
-	ldr r6, =0xFC000000
+	ldr r6, =0xFFFFF000
 	and r5, r6
-	ldr r6, =0x02AAAAAA
+	ldr r6, =0x00000AAA
 	orr r5, r6
 	str r5, [r4, #GPIOx_MODER]
 
-	ldr r5, [r4, #GPIOx_AFRL]
-	ldr r6, =0x00000000
+	ldr r5, [r4, #GPIOx_PUPDR]
+	ldr r6, =0xFFFFF000
 	and r5, r6
-	ldr r6, =0xBBBBBBBB
+	str r5, [r4, #GPIOx_PUPDR]
+
+	ldr r5, [r4, #GPIOx_AFRL]
+	ldr r6, =0xFF000000
+	and r5, r6
+	ldr r6, =0x00BBBBBB
 	orr r5, r6
 	str r5, [r4, #GPIOx_AFRL]
-
-	ldr r5, [r4, #GPIOx_AFRH]
-	ldr r6, =0xFFF00000
-	and r5, r6
-	ldr r6, =0x000BBBBB
-	orr r5, r6
-	str r5, [r4, #GPIOx_AFRH]
 
 	pop { r4, r5, r6, pc }
 
-
-	.type	init_lcd_d %function
-	.global init_lcd_d
-init_lcd_d:
+.type	lcd_clear_ram %function
+lcd_clear_ram:
 	push { r4, r5, r6, lr }
 
-	ldr r4, =GPIOD_BASE
-	ldr r5, [r4, #GPIOx_MODER]
-	ldr r6, =0x0000FFCF
-	and r5, r6
-	ldr r6, =0xAAAA0020
-	orr r5, r6
-	str r5, [r4, #GPIOx_MODER]
-
-	ldr r5, [r4, #GPIOx_AFRL]
-	ldr r6, =0xFFFFF0FF
-	and r5, r6
-	ldr r6, =0x00000B00
-	orr r5, r6
-	str r5, [r4, #GPIOx_AFRL]
-
-	ldr r5, [r4, #GPIOx_AFRH]
-	ldr r6, =0x00000000
-	and r5, r6
-	ldr r6, =0xBBBBBBBB
-	orr r5, r6
-	str r5, [r4, #GPIOx_AFRH]
-
-	pop { r4, r5, r6, pc }
-
-
-	.type	init_lcd_e %function
-	.global	init_lcd_e
-init_lcd_e:
-	push { r4, r5, r6, lr }
-
-	ldr r4, =GPIOE_BASE
-	ldr r5, [r4, #GPIOx_MODER]
-	ldr r6, =0xFFFFFF00
-	and r5, r6
-	ldr r6, =0x000000AA
-	orr r5, r6
-	str r5, [r4, #GPIOx_MODER]
-
-	ldr r5, [r4, #GPIOx_AFRL]
-	ldr r6, =0xFFFF0000
-	and r5, r6
-	ldr r6, =0x0000BBBB
-	orr r5, r6
-	str r5, [r4, #GPIOx_AFRL]
+	ldr r4, =LCD_BASE + LCD_RAM
+	ldr r5, =0x00
+	ldr r6, =0x0F
+clear_ram_loop:
+	str r5, [r4], #4
+	subs r7, #1
+	bne clear_ram_loop
 
 	pop { r4, r5, r6, pc }
--- stm32l4-asm/Src/main.s
+++ stm32l4-asm/Src/main.s
@@ -12,85 +12,9 @@ struct:	.space 24
 .global main
 
 main:
-	bl c_function1
-	bl asm_function2
-	bl c_function3
-	bl asm_function4
+	bl init_lcd
+
+loop:
+	bl lcd_display_bar
+	b loop
 __end: b __end
-
-.type	asm_function1 %function
-.global	asm_function1
-
-asm_function1:
-	push { r4, r5, lr }
-
-	ldr r4, [sp, #12]
-	ldr r5, [sp, #16]
-
-	pop { r4, r5, pc }
-
-
-.type	asm_function2 %function
-.global	asm_function2
-
-asm_function2:
-	push { r4, lr }
-	ldr r0, =1
-	ldr r1, =2
-	ldr r2, =3
-	ldr r3, =4
-
-	ldr r4, =6
-	str r4, [sp, #-4]!
-	ldr r4, =5
-	str r4, [sp, #-4]!
-
-	bl c_function2
-
-	add sp, #8
-
-	pop { r4, pc }
-
-.type	asm_function3 %function
-.global	asm_function3
-
-asm_function3:
-	push { r4, lr }
-
-	ldr r4, =1
-	str r4, [r0]
-	ldr r4, =2
-	str r4, [r0, #4]
-	ldr r4, =3
-	str r4, [r0, #8]
-	ldr r4, =4
-	str r4, [r0, #12]
-	ldr r4, =5
-	str r4, [r0, #16]
-	ldr r4, =6
-	str r4, [r0, #20]
-
-	pop { r4, pc }
-
-.type	asm_function4 %function
-.global	asm_function4
-
-asm_function4:
-	push { r4, r5, r6, lr }
-
-	sub sp, #24
-	mov r0, sp
-
-	bl c_function4
-
-	ldr r1, [r0]
-	ldr r2, [r0, #4]
-	ldr r3, [r0, #8]
-	ldr r4, [r0, #12]
-	ldr r5, [r0, #16]
-	ldr r6, [r0, #20]
-
-	add sp, #24
-
-	pop { r4, r5, r6, pc }
-
