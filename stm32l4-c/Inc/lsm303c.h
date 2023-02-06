e19dba30900a5b9008f982061a9fc55dc58bba18
Jakob <Jakob@DESKTOP-9G0HBHJ>
2023-02-06 07:24:49 +0100

    Adding support for Gyro and SPI

---
+++ stm32l4-c/Inc/lsm303c.h
@@ -0,0 +1,13 @@
+/*
+ * lsm303c.h
+ *
+ *  Created on: 6. feb. 2023
+ *      Author: Jakob
+ */
+
+#ifndef LSM303C_H_
+#define LSM303C_H_
+
+
+
+#endif /* LSM303C_H_ */
---
+++ stm32l4-c/Src/lsm303c.c
@@ -0,0 +1,8 @@
+/*
+ * lsm303c.c
+ *
+ *  Created on: 6. feb. 2023
+ *      Author: Jakob
+ */
+
+#include "lsm303c.h"
